33fe131f3b53ef77c4dfe653aee1c3b16ef9e905
sahulkodali <sahulkodali@icloud.com>
2021-01-22 17:20:53 -0500

    initial checkin

---
+++ src/main/java/com/myapi/spring/onlinetutorial/OnlinetutorialApplication.java
@@ -0,0 +1,13 @@
+package com.myapi.spring.onlinetutorial;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlinetutorialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlinetutorialApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapi/spring/onlinetutorial/OnlinetutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapi.spring.onlinetutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlinetutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
